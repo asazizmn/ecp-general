6d52c4a1bce0201d971d9360b022b05c50ecc933
S. Aziz <asazizmn@gmail.com>
2025-05-10 09:57:11 +0800

    add eclipse based java 'Salam Dunya' project

---
+++ SalamDunya/src/SalamDunya.java
@@ -0,0 +1,11 @@
+/**
+ * SalamDunya
+ * - Simple class that outputs, Salam
+ */
+
+
+public class SalamDunya {
+	public static void main( String[] args ) {
+		System.out.println( "Salam, Dunya!" );
+	}
+}
